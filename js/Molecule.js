dc2581a382c3dc40d6d7033e490159c792e66cfb
Patrick Graham <pabreetzio@gmail.com>
2014-02-26 23:33:31 -0500

    Adding Molecule Class. Also testing VS Git integration with this commit.

---
+++ js/Molecule.js
@@ -0,0 +1,20 @@
+﻿var Molecule = function (args) {
+    var self = this;
+    self.molecularFormula = '';
+    self.composition = {};
+    if (typeof args == 'string') self.molecularFormula = args;
+    else { throw 'not implemented yet'; }
+    self.composition = Molecule.getComposition(self.molecularFormula);
+}
+Molecule.getComposition = function (formula) {
+    var composition = {};
+    var elementGroups = formula.match(/[A-Z]{1}[a-z]{0,1}\d*/g);
+    _.each(elementGroups, function (elementGroup) {
+        var element = elementGroup.match(/[A-Z][a-z]{0,1}/)[0];
+        var quantity = elementGroup.match(/\d/)[0];
+        typeof composition[element] === 'number' ?
+            composition[element] += 5 :
+            composition[element] = 5;
+    });
+    return composition;
+}
