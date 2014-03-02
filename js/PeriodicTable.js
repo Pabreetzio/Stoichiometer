e7681f54f656a8fe74c7ced24d9c5fb103ec0d29
Patrick Graham <pabreetzio@gmail.com>
2014-03-01 22:10:13 -0500

    Did some basic work on creating periodic table. showin on tests page.

--- js/PeriodicTable.js
+++ js/PeriodicTable.js
@@ -1,12 +1,33 @@
 ﻿var PeriodicTable = function (args) {
     var self = this;
     var defaultTemplateLocation = "../template/periodicTableCell.html";
-    var defaultDataLocation = "../data/TableOfElements.json";
-    var periodicTable = $('<div>', { text: 'blah' });
-    var periodicTableCell;
-    $.get(defaultTemplateLocation, function (templateText) {
-        periodicTableCell = $(templateText);
+    var defaultDataLocation = "../data/TableOfElements.js";
+    var periodicTable = $('<div>', { });
+    var periodicTableCellTemplate;
+    var gettingTemplate = $.get(defaultTemplateLocation, function (templateText) {
+        periodicTableCellTemplate = templateText;
     });
-
-    return periodicTable;
+    var elements;
+    var gettingElements = $.get(defaultDataLocation, function(tableOfElements){
+        elements = $.parseJSON(tableOfElements);
+    });
+    var groups = _.range(1, 19);
+    var periods = _.range(1, 8);
+    $.when(gettingElements, gettingTemplate).done(function () {
+        _.forEach(periods, function (period) {
+            _.forEach(groups, function (group) {
+                var tableCell = $(periodicTableCellTemplate);
+                var element = _.find(elements, { "Period": period, "Group": group });
+                if (element) {
+                    tableCell.find('.periodic-table-element-number').text(element.AtomicNumber);
+                    tableCell.find('.periodic-table-element-symbol').text(element.Symbol);
+                    tableCell.find('.periodic-table-element-name').text(element.Name);
+                    tableCell.find('.periodic-table-element-mass').text(element.AtomicMass);
+                }
+                periodicTable.append(tableCell);
+            });
+            periodicTable.append($('<br>'));
+        });
+    });
+    $('#periodic-table').html(periodicTable);
 }
